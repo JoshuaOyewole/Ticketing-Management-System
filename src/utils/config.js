8ef8c873974bf321651d6094963bc382b649b6e4
joshua Oyewole <joshpp2013@gmail.com>
2024-02-15 15:59:55 +0100

    Added firebase config file

---
+++ src/utils/config.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: "flight-booking-c01fc.firebaseapp.com",
+  projectId: "flight-booking-c01fc",
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+  measurementId: import.meta.env.VITE_measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export {db};  
